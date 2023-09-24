7991083f67d6dd469b36dc3aaae7910fbea36fc3
Kristyna93 <145651148+Kristyna93@users.noreply.github.com>
2023-09-24 12:32:42 +0200

    Úkol 1

---
+++ ukol1.py
@@ -0,0 +1,15 @@
+jmeno = "Kristýna"
+domena = "@czechitas.cz"
+print(jmeno + domena)
+
+
+
+#bonus úkolu, ale nedodělaný, nevěděla jsem si rady jak vložit tečky za iniciály
+
+jmeno = input("Napište prosím své jméno: ")
+prijmeni = input("Napište prosím své příjmení: ")
+jmeno_a_prijmeni = (jmeno) + (" ") + prijmeni
+print (jmeno_a_prijmeni.upper())
+print (jmeno_a_prijmeni.lower())
+inicialy = jmeno[0] + prijmeni[0]
+print(inicialy.upper())
